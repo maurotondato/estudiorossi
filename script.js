4d5d93367466c1ad24fe624cd2121229ed76f8be
maurotondato <134478369+maurotondato@users.noreply.github.com>
2024-05-16 13:27:30 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var banner = document.getElementById("banner");
     banner.classList.add("loaded");
 });
+// Desplazamiento suave al cargar la página
+window.addEventListener('load', function() {
+    // Calculamos la altura del carrusel
+    var carouselHeight = document.getElementById('carouselSection').offsetTop;
+
+    // Realizamos un desplazamiento suave hacia la altura del carrusel
+    window.scrollTo({
+        top: carouselHeight,
+        behavior: 'smooth'
+    });
+
+    // Después de un tiempo corto, realizamos otro desplazamiento suave hacia arriba
+    setTimeout(function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, 1000); // 1000 milisegundos = 1 segundo (puedes ajustar este valor según lo desees)
+});
